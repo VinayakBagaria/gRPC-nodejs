fa73fc750b974ee61ddd5e681b953acb312a4cc9
techentertainer <vinayak.bagaria@curofy.com>
2020-02-08 13:40:19 +0530

    client file

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const grpc = require("grpc");
-const uuid = require("uuid/v1");
+const grpc = require('grpc');
+const uuid = require('uuid/v1');
 
-const todoproto = grpc.load("todo.proto");
+const todoproto = grpc.load('todo.proto');
 
 const server = new grpc.Server();
 
-server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure());
+server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure());
 
-console.log("server is running at http://127.0.0.1:50051");
+console.log('server is running at http://127.0.0.1:50051');
 
 server.start();
 
