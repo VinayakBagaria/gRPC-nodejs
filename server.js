73222242c128013fca31f39c3268f41eb0971bcb
techentertainer <vinayak.bagaria@curofy.com>
2020-02-08 13:35:25 +0530

    basic server file, nothing initialized

---
+++ server.js
@@ -0,0 +1,12 @@
+const grpc = require('grpc');
+const uuid = require('uuid/v1');
+
+const todoproto = grpc.load('todo.proto');
+
+const server = new grpc.Server();
+
+server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure());
+
+console.log('server is running at http://127.0.0.1:50051');
+
+server.start();
