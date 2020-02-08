9463cecaf999420cdbd01c743b2380fe6a8b5bc2
techentertainer <vinayak.bagaria@curofy.com>
2020-02-08 13:37:13 +0530

    service configured with proto

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const grpc = require('grpc');
-const uuid = require('uuid/v1');
+const grpc = require("grpc");
+const uuid = require("uuid/v1");
 
-const todoproto = grpc.load('todo.proto');
+const todoproto = grpc.load("todo.proto");
 
 const server = new grpc.Server();
 
-server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure());
+server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure());
 
-console.log('server is running at http://127.0.0.1:50051');
+console.log("server is running at http://127.0.0.1:50051");
 
 server.start();
+
+server.addService(todoproto.TodoService.service, {
+  list: (_, callback) => {
+    callback(null, todos);
+  }
+});
